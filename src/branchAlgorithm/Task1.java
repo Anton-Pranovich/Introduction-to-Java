d2ac0427a5b1fadcfbbf8885944e63c38f575c8e
Anton-Pranovich <anton.pronovich@gmail.com>
2021-10-23 19:03:52 +0300

    add branch algorithm tasks

---
+++ src/branchAlgorithm/Task1.java
@@ -0,0 +1,28 @@
+package branchAlgoritm;
+
+import java.util.Scanner;
+
+/**
+ * Даны два угла треугольника (в градусах). Определить, существует ли такой
+ * треугольник, и если да, то будет ли он прямоугольным.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input a");
+        int firstCorner = sc.nextInt();
+        System.out.println("input b");
+        int secondCorner = sc.nextInt();
+        int thirdCorner = firstCorner + secondCorner;
+        if (thirdCorner < 180) {
+            if (thirdCorner == 90) {
+                System.out.println("Трекгольник прямоугольный");
+            } else {
+                System.out.println("Треугольник не прямоугольный");
+            }
+        } else {
+            System.out.println("Нет такого треугольника");
+        }
+        sc.close();
+    }
+}
---
+++ src/branchAlgorithm/Task5.java
@@ -0,0 +1,22 @@
+package branchAlgoritm;
+
+import java.util.Scanner;
+
+/**
+ * Вычислить значение функции
+ */
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input x");
+        int x = sc.nextInt();
+        double y;
+        if (x <= 3) {
+            y = ((Math.pow(x, 2)) - (3 * x) + 9);
+        } else {
+            y = 1 / ((Math.pow(x, 3)) + 6);
+        }
+        System.out.println(y);
+        sc.close();
+    }
+}
