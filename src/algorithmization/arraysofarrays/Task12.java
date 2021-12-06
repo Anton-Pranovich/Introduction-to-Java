9cc2cd454c8d5218a0a57101bd77356ec68c9c66
Anton-Pranovich <anton.pronovich@gmail.com>
2021-12-06 12:38:53 +0300

    add Task12

--- src/algorithmization/arraysofarrays/Task12.java
+++ src/algorithmization/arraysofarrays/Task12.java
@@ -5,6 +5,59 @@ package algorithmization.arraysofarrays;
  */
 public class Task12 {
     public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {4, 3, 2, 1},
+                {5, 6, 7, 8},
+                {8, 7, 6, 5}
+        };
+        System.out.println("Origin matrix:");
+        printMatrix(matrix);
 
+        boolean isSorted = false;
+        int buffer;
+
+        for (int row = 0; row < matrix.length; row++) {
+            while (!isSorted) {
+                isSorted = true;
+                for (int col = 0; col < matrix[row].length - 1; col++) {
+                    if (matrix[row][col] > matrix[row][col + 1]) {
+                        isSorted = false;
+                        buffer = matrix[row][col];
+                        matrix[row][col] = matrix[row][col + 1];
+                        matrix[row][col + 1] = buffer;
+                    }
+                }
+            }
+            isSorted = false;
+        }
+        System.out.println("\nAscending sort matrix");
+        printMatrix(matrix);
+        for (int row = 0; row < matrix.length; row++) {
+            while (!isSorted) {
+                isSorted = true;
+                for (int col = 0; col < matrix[row].length - 1; col++) {
+                    if (matrix[row][col] < matrix[row][col + 1]) {
+                        isSorted = false;
+                        buffer = matrix[row][col];
+                        matrix[row][col] = matrix[row][col + 1];
+                        matrix[row][col + 1] = buffer;
+                    }
+                }
+            }
+            isSorted = false;
+        }
+        System.out.println("\nDescending sort");
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + "\t");
+            }
+            System.out.println();
+        }
     }
 }
